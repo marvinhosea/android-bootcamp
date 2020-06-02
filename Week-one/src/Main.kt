f148f59e47155ebadea75110367fb3733d1a8a60
kodaTheCoder <marvincollins14@gmail.com>
2020-06-02 21:35:02 +0300

    Added suits array

--- Week-one/src/Main.kt
+++ Week-one/src/Main.kt
@@ -6,5 +6,18 @@ data class Card(
 
 fun main() {
     val card = Card("10", 'C')
-    println(card)
+
+    val cardCollection = mutableSetOf<Card>()
+
+    val suits = arrayOf('\u2663', '\u2660', '\u2666', '\u2665')
+    println(suits.size)
+    val blackJackRange = 2..10
+
+    for (pip in blackJackRange){
+        for (suit in suits){
+            cardCollection.add(Card(pip.toString(), suit))
+        }
+    }
+
+    println(cardCollection)
 }
