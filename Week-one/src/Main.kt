87a2e8991aac6a12ba833354493910e20467f96e
kodaTheCoder <marvincollins14@gmail.com>
2020-06-02 21:47:39 +0300

    Created createDeck function

--- Week-one/src/Main.kt
+++ Week-one/src/Main.kt
@@ -3,21 +3,22 @@ data class Card(
         val suit: Char
 )
 
-
 fun main() {
     val card = Card("10", 'C')
 
-    val cardCollection = mutableSetOf<Card>()
+    fun createDeck(): MutableSet<Card>{
+        val cardCollection = mutableSetOf<Card>()
+        val suits = arrayOf('\u2663', '\u2660', '\u2666', '\u2665')
 
-    val suits = arrayOf('\u2663', '\u2660', '\u2666', '\u2665')
-    println(suits.size)
-    val blackJackRange = 2..10
+        val blackJackRange = 2..10
 
-    for (pip in blackJackRange){
-        for (suit in suits){
-            cardCollection.add(Card(pip.toString(), suit))
+        for (pip in blackJackRange){
+            for (suit in suits){
+                cardCollection.add(Card(pip.toString(), suit))
+            }
         }
+
+        return cardCollection
     }
 
-    println(cardCollection)
 }
