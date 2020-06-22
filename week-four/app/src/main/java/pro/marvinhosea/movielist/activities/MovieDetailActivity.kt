991fb960ed7e2b0c9e1ed9f5477854988940a0b8
kodaTheCoder <marvincollins14@gmail.com>
2020-06-22 16:27:32 +0300

    Fixed movie detail activity error and finalized on detail activity functionalities

--- week-four/app/src/main/java/pro/marvinhosea/movielist/activities/MovieDetailActivity.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/activities/MovieDetailActivity.kt
@@ -2,11 +2,28 @@ package pro.marvinhosea.movielist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_movie_detail.*
+import pro.marvinhosea.movielist.MainActivity
 import pro.marvinhosea.movielist.R
+import pro.marvinhosea.movielist.models.Movie
 
 class MovieDetailActivity : AppCompatActivity() {
+
+    lateinit var movie: Movie
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        movie = intent.getParcelableExtra(MainActivity.INTENT_MOVIE_KEY) as Movie
+        title = movie.name
+
+        detail_image_view.setImageResource(movie.posterId)
+        detail_movie_name.text = getString(R.string.movie_name_text, movie.name)
+        detail_movie_genre.text = getString(R.string.movie_genre, movie.genre)
+        detail_release_date.text = getString(R.string.movie_release_date_text, movie.releaseDate)
+        detail_movie_summary.text = movie.summary
     }
 }
