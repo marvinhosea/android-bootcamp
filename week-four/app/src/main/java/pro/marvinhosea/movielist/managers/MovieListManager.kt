2ebed0841a82c76a9c669d8cd8bbede49033fabc
kodaTheCoder <marvincollins14@gmail.com>
2020-06-22 09:44:41 +0300

    Added movie detail activity

--- week-four/app/src/main/java/pro/marvinhosea/movielist/managers/MovieListManager.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/managers/MovieListManager.kt
@@ -1,5 +1,13 @@
 package pro.marvinhosea.movielist.managers
 
+import android.preference.PreferenceManager
+
 class MovieListManager {
-    
+    /**
+     * Save movie to shared preferences
+     */
+    fun saveMovieList(){
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences().edit()
+        sharedPrefs.putStringSet();
+    }
 }
