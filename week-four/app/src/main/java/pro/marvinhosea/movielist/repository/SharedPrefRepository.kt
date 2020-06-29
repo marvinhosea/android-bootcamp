85907a0f89b6aa325b3121ec9b8db23bd8e1f02d
kodaTheCoder <marvincollins14@gmail.com>
2020-06-30 01:11:40 +0300

    Finalized user logout implementation

--- week-four/app/src/main/java/pro/marvinhosea/movielist/repository/SharedPrefRepository.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/repository/SharedPrefRepository.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.preference.PreferenceManager
 
 object SharedPrefRepository: UserRepository {
-    
+
     private lateinit var applicationContext: Context
     const val USERNAME = "USERNAME"
     const val USER_PASSWORD = "USER_PASSWORD"
+    const val USER_IS_LOGGED_IN = "USER_IS_LOGGED_IN"
 
     private fun sharedPreps() = PreferenceManager.getDefaultSharedPreferences(applicationContext)
 
@@ -16,10 +17,11 @@ object SharedPrefRepository: UserRepository {
     }
 
     override fun registerUser(username: String, password: String) {
-        val editor = sharedPreps().edit();
+        val editor = sharedPreps().edit()
 
         editor.putString(USERNAME, username).apply()
         editor.putString(USER_PASSWORD, password).apply()
+        editor.putBoolean(USER_IS_LOGGED_IN, true).apply()
     }
 
     override fun loginUser(username: String, password: String): Boolean {
@@ -34,10 +36,17 @@ object SharedPrefRepository: UserRepository {
             return  false
         }
 
+        val editor = sharedPreps().edit()
+        editor.putBoolean(USER_IS_LOGGED_IN, true).apply()
+
         return true
     }
 
-    override fun clearUser() {
-        TODO("Not yet implemented")
+    override fun logoutUser() {
+        sharedPreps().edit().clear().apply()
+    }
+
+    override fun isUserLoggedIn(): Boolean {
+        return sharedPreps().getBoolean(USER_IS_LOGGED_IN, false)
     }
 }
--- week-four/app/src/main/java/pro/marvinhosea/movielist/repository/UserRepository.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/repository/UserRepository.kt
@@ -3,5 +3,6 @@ package pro.marvinhosea.movielist.repository
 interface UserRepository {
     fun registerUser(username: String, password: String)
     fun loginUser(username: String, password: String): Boolean
-    fun clearUser()
+    fun logoutUser()
+    fun isUserLoggedIn(): Boolean
 }
