16c2adb0877df4efcca4cb8f6b4af6aa3845bc4a
kodaTheCoder <marvincollins14@gmail.com>
2020-07-02 21:46:23 +0300

    Update MovieDatabase.kt

--- week-four/app/src/main/java/pro/marvinhosea/movielist/models/MovieDatabase.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/models/MovieDatabase.kt
@@ -2,21 +2,30 @@ package pro.marvinhosea.movielist.models
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 @Database(entities = [Movie::class], version = 1, exportSchema = false)
 public abstract class MovieDatabase : RoomDatabase(){
     abstract fun movieDao():MovieDao
 
-    companion object{
-        var INSTANCE: MovieDatabase? = null
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
 
-        fun getDatabase(context: Context): MovieDatabase {
-            val tempInstance = INSTANCE
-
-            if (tempInstance != null){
-                return tempInstance
+        fun getDatabase(context: Context): MovieDatabase? {
+            when (INSTANCE) {
+                null -> INSTANCE = Room.databaseBuilder(
+                    context, MovieDatabase::class.java,
+                    "movielist"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
             }
+            return INSTANCE
+        }
 
+        fun destroyInstance() {
+            INSTANCE = null
         }
     }
 }
