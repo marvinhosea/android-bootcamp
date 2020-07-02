788d4f6c36e3ada591d3a597bc43c24571581c48
kodaTheCoder <marvincollins14@gmail.com>
2020-07-02 20:31:22 +0300

    Updated package name MovieViewHolder.kt

--- week-four/app/src/main/java/pro/marvinhosea/movielist/viewmodels/MovieViewHolder.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/viewmodels/MovieViewHolder.kt
@@ -1,4 +1,4 @@
-package pro.marvinhosea.movielist.`view-models`
+package pro.marvinhosea.movielist.viewmodels
 
 import android.view.View
 import android.widget.ImageView
@@ -6,10 +6,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pro.marvinhosea.movielist.R
 
-class MovieViewHolder(movieView: View): RecyclerView.ViewHolder(movieView) {
-    val moviePosterImageView = movieView.findViewById<ImageView>(R.id.image_view)
-    val movieReleaseDate = movieView.findViewById<TextView>(R.id.release_date)
-    val movieName = movieView.findViewById<TextView>(R.id.movie_name)
-    val movieSummary = movieView.findViewById<TextView>(R.id.movie_summary)
-    val movieGenre = movieView.findViewById<TextView>(R.id.movie_genre)
+class MovieViewHolder(movieView: View) : RecyclerView.ViewHolder(movieView) {
+    val moviePosterImageView: ImageView = movieView.findViewById(R.id.image_view)
+    val movieReleaseDate: TextView = movieView.findViewById(R.id.release_date)
+    val movieName: TextView = movieView.findViewById(R.id.movie_name)
+    val movieSummary: TextView = movieView.findViewById(R.id.movie_summary)
+    val movieGenre: TextView = movieView.findViewById(R.id.movie_genre)
 }
