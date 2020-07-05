1dc42fb44ee7e5879dbfef41c5b338cefa22cda1
kodaTheCoder <marvincollins14@gmail.com>
2020-07-05 20:54:06 +0300

    Updated MovieDao.kt to utilize coroutines Added storeMovies, storeMovie methods Replaced getAllMovies with fetch movies

--- week-four/app/src/main/java/pro/marvinhosea/movielist/data/dao/MovieDao.kt
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/data/dao/MovieDao.kt
@@ -1,21 +1,27 @@
 package pro.marvinhosea.movielist.data.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
+import androidx.room.*
 import androidx.room.OnConflictStrategy.IGNORE
-import androidx.room.Query
 import pro.marvinhosea.movielist.data.models.Movie
 
 @Dao
 interface MovieDao {
     @Insert(onConflict = IGNORE)
-    suspend fun insertSingleMovie(movie: Movie)
+    suspend fun storeMovie(movie: Movie)
+
+    @Insert(onConflict = IGNORE)
+    fun storeMovies(movies: List<Movie>)
+
+    @Update
+    fun updateMovie(movie: Movie)
 
     @Delete
-    fun deleteMovie(vararg movie: Movie)
+    suspend fun deleteMovie(movie: Movie)
 
-    @Query("SELECT * FROM movies_table")
-    fun getAllMovies(): LiveData<List<Movie>>
+    @Query("SELECT * FROM movies WHERE id = :id")
+    fun fetchMovieById(id: String): LiveData<Movie>
+
+    @Query("SELECT * FROM movies")
+    fun fetchMovies(): LiveData<List<Movie>>
 }
