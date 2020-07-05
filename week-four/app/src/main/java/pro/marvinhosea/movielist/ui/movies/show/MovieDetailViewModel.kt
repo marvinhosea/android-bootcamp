2471c427cdbc2136eeffa604bc158c54ebcd7e5b
kodaTheCoder <marvincollins14@gmail.com>
2020-07-05 20:54:26 +0300

    Update MovieDetailViewModel.kt

---
+++ week-four/app/src/main/java/pro/marvinhosea/movielist/ui/movies/show/MovieDetailViewModel.kt
@@ -0,0 +1,23 @@
+package pro.marvinhosea.movielist.ui.movies.show
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pro.marvinhosea.movielist.data.models.Movie
+import pro.marvinhosea.movielist.repository.MoviesRepository
+
+class MovieDetailViewModel(application: Application) : AndroidViewModel(application) {
+    private val movieRepository by lazy {
+        MoviesRepository(application)
+    }
+
+    fun getMovie(movieId: String): LiveData<Movie> {
+        return movieRepository.getMovie(movieId)
+    }
+
+    fun deleteMovie(movie: Movie) {
+        viewModelScope.launch { movieRepository.deleteMovie(movie) }
+    }
+}
