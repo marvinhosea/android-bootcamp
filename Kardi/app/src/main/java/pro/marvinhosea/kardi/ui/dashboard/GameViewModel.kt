012d476eed0f4c380e351b1d327f99381af879a6
kodaTheCoder <marvincollins14@gmail.com>
2020-08-16 18:01:46 +0300

    Personal project

---
+++ Kardi/app/src/main/java/pro/marvinhosea/kardi/ui/dashboard/GameViewModel.kt
@@ -0,0 +1,13 @@
+package pro.marvinhosea.kardi.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ Kardi/app/src/main/java/pro/marvinhosea/kardi/ui/notifications/AccountFragment.kt
@@ -0,0 +1,31 @@
+package pro.marvinhosea.kardi.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import pro.marvinhosea.kardi.R
+
+class AccountFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: AccountViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(AccountViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
